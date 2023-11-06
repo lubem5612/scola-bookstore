33d0faaf82788176d822ebc902ed37f41b00da60
Jonathan Audu <jaudu2@gmail.com>
2023-11-06 11:22:25 +0000

    Jon dev

--- database/migrations/2018_01_01_463511_create_publishers_table.php
+++ database/migrations/2018_01_01_463511_create_publishers_table.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreatePublishersTable extends Migration
 {
     public function up()
     {
@@ -25,4 +25,4 @@ return new class extends Migration
         Schema::dropIfExists('publishers');
 
     }
-};
+}
