78cfaf0f1bb5bb914d80c8800893babd336e0202
Gabriel Hishen Michael <michaelgabriel165@gmail.com>
2024-02-02 14:59:07 +0100

    routes names

--- routes/api.php
+++ routes/api.php
@@ -31,11 +31,11 @@ $prefix = !empty(config('endpoints.prefix')) ? config('endpoints.prefix') : 'gen
  */
 
 Route::prefix($prefix)->as('bookstore.')->group(function () {
-    Route::get('{endpoint}', [ResourceController::class, 'index'])->name('index');
-    Route::post('{endpoint}', [ResourceController::class, 'store'])->name('store');
-    Route::get('{endpoint}/{id}', [ResourceController::class, 'show'])->name('show');
-    Route::match(['post', 'put', 'patch'], '{endpoint}/{id}', [ResourceController::class, 'update'])->name('update');
-    Route::delete('{endpoint}/{id}', [ResourceController::class, 'destroy'])->name('delete');
+    Route::get('{endpoint}', [ResourceController::class, 'index']);
+    Route::post('{endpoint}', [ResourceController::class, 'store']);
+    Route::get('{endpoint}/{id}', [ResourceController::class, 'show']);
+    Route::match(['post', 'put', 'patch'], '{endpoint}/{id}', [ResourceController::class, 'update']);
+    Route::delete('{endpoint}/{id}', [ResourceController::class, 'destroy']);
 });
 
 
@@ -64,48 +64,48 @@ Route::as('bookstore.')->group(function () {
 
     //Sales Route
     Route::prefix('sales')->group(function () {
-        Route::get('/', [SalesController::class, 'index'])->name('index');
-        Route::get('/{id}', [SalesController::class, 'show'])->name('show');
+        Route::get('/', [SalesController::class, 'index']);
+        Route::get('/{id}', [SalesController::class, 'show']);
         Route::get('purchase/{id}', [SalesController::class, 'userPurchase'])->name('purchase');
     });
 
 
     //Pickups Route
     Route::prefix('pickups')->group(function () {
-        Route::get('/', [PickupController::class, 'index'])->name('index');
-        Route::get('/{id}', [PickupController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [PickupController::class, 'update'])->name('update');
+        Route::get('/', [PickupController::class, 'index']);
+        Route::get('/{id}', [PickupController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [PickupController::class, 'update']);
     });
 
 
  //Become a Reviewer Route
     Route::prefix('reviewer_requests')->group(function () {
-        Route::get('/', [ReviewerRequestController::class, 'index'])->name('index');
-        Route::post('/', [ReviewerRequestController::class, 'store'])->name('store');
-        Route::get('/{id}', [ReviewerRequestController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ReviewerRequestController::class, 'update'])->name('update');
-        Route::delete('/{id}', [ReviewerRequestController::class, 'destroy'])->name('delete');
+        Route::get('/', [ReviewerRequestController::class, 'index']);
+        Route::post('/', [ReviewerRequestController::class, 'store']);
+        Route::get('/{id}', [ReviewerRequestController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ReviewerRequestController::class, 'update']);
+        Route::delete('/{id}', [ReviewerRequestController::class, 'destroy']);
     });
 
 
     //Books Route
     Route::prefix('books')->group(function () {
-        Route::get('/', [BookController::class, 'index'])->name('index');
-        Route::post('/', [BookController::class, 'store'])->name('store');
-        Route::get('/{id}', [BookController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [BookController::class, 'update'])->name('update');
-        Route::delete('/{id}', [BookController::class, 'destroy'])->name('delete');
+        Route::get('/', [BookController::class, 'index']);
+        Route::post('/', [BookController::class, 'store']);
+        Route::get('/{id}', [BookController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [BookController::class, 'update']);
+        Route::delete('/{id}', [BookController::class, 'destroy']);
 
     });
 
 
         //Carts Route
     Route::prefix('carts')->group(function () {
-        Route::get('/', [ CartController::class, 'index'])->name('index');
-        Route::post('/', [CartController::class, 'store'])->name('store');
-        Route::get('/{userId}', [CartController::class, 'show'])->name('show');
+        Route::get('/', [ CartController::class, 'index']);
+        Route::post('/', [CartController::class, 'store']);
+        Route::get('/{userId}', [CartController::class, 'show']);
         Route::delete('/{cartItemId}', [CartController::class, 'removeItem'])->name('removeItem');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [CartController::class, 'update'])->name('update');
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [CartController::class, 'update']);
         Route::delete('clear/{userId}', [CartController::class, 'clearCart'])->name('clear');
 
     });
@@ -113,76 +113,76 @@ Route::as('bookstore.')->group(function () {
       
     //Conference Paper Route
     Route::prefix('papers')->group(function () {
-        Route::get('/', [ConferencePaperController::class, 'index'])->name('index');
-        Route::post('/', [ConferencePaperController::class, 'store'])->name('store');
-        Route::get('/{id}', [ConferencePaperController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ConferencePaperController::class, 'update'])->name('update');
-        Route::delete('/{id}', [ConferencePaperController::class, 'destroy'])->name('delete');
+        Route::get('/', [ConferencePaperController::class, 'index']);
+        Route::post('/', [ConferencePaperController::class, 'store']);
+        Route::get('/{id}', [ConferencePaperController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ConferencePaperController::class, 'update']);
+        Route::delete('/{id}', [ConferencePaperController::class, 'destroy']);
 
     });
 
 
         //Articles Route
     Route::prefix('articles')->group(function () {
-        Route::get('/', [ArticleController::class, 'index'])->name('index');
-        Route::post('/', [ArticleController::class, 'store'])->name('store');
-        Route::get('/{id}', [ArticleController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ArticleController::class, 'update'])->name('update');
-        Route::delete('/{id}', [ArticleController::class, 'destroy'])->name('delete');
+        Route::get('/', [ArticleController::class, 'index']);
+        Route::post('/', [ArticleController::class, 'store']);
+        Route::get('/{id}', [ArticleController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ArticleController::class, 'update']);
+        Route::delete('/{id}', [ArticleController::class, 'destroy']);
 
     });
 
 
         //festchrisfts Route
     Route::prefix('festchrisfts')->group(function () {
-        Route::get('/', [FestchrisftController::class, 'index'])->name('index');
-        Route::post('/', [FestchrisftController::class, 'store'])->name('store');
-        Route::get('/{id}', [FestchrisftController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [FestchrisftController::class, 'update'])->name('update');
-        Route::delete('/{id}', [FestchrisftController::class, 'destroy'])->name('delete');
+        Route::get('/', [FestchrisftController::class, 'index']);
+        Route::post('/', [FestchrisftController::class, 'store']);
+        Route::get('/{id}', [FestchrisftController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [FestchrisftController::class, 'update']);
+        Route::delete('/{id}', [FestchrisftController::class, 'destroy']);
 
     });
 
 
             //Journal Route
     Route::prefix('journals')->group(function () {
-        Route::get('/', [JournalController::class, 'index'])->name('index');
-        Route::post('/', [JournalController::class, 'store'])->name('store');
-        Route::get('/{id}', [JournalController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [JournalController::class, 'update'])->name('update');
-        Route::delete('/{id}', [JournalController::class, 'destroy'])->name('delete');
+        Route::get('/', [JournalController::class, 'index']);
+        Route::post('/', [JournalController::class, 'store']);
+        Route::get('/{id}', [JournalController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [JournalController::class, 'update']);
+        Route::delete('/{id}', [JournalController::class, 'destroy']);
 
     });
 
 
 //Monograph route
         Route::prefix('monographs')->group(function () {
-        Route::get('/', [MonographController::class, 'index'])->name('index');
-        Route::post('/', [MonographController::class, 'store'])->name('store');
-        Route::get('/{id}', [MonographController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [MonographController::class, 'update'])->name('update');
-        Route::delete('/{id}', [MonographController::class, 'destroy'])->name('delete');
+        Route::get('/', [MonographController::class, 'index']);
+        Route::post('/', [MonographController::class, 'store']);
+        Route::get('/{id}', [MonographController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [MonographController::class, 'update']);
+        Route::delete('/{id}', [MonographController::class, 'destroy']);
 
     });
 
 
     //Research Resource route
         Route::prefix('research_resources')->group(function () {
-        Route::get('/', [ResearchResourceController::class, 'index'])->name('index');
-        Route::post('/', [ResearchResourceController::class, 'store'])->name('store');
-        Route::get('/{id}', [ResearchResourceController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ResearchResourceController::class, 'update'])->name('update');
-        Route::delete('/{id}', [ResearchResourceController::class, 'destroy'])->name('delete');
+        Route::get('/', [ResearchResourceController::class, 'index']);
+        Route::post('/', [ResearchResourceController::class, 'store']);
+        Route::get('/{id}', [ResearchResourceController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ResearchResourceController::class, 'update']);
+        Route::delete('/{id}', [ResearchResourceController::class, 'destroy']);
 
     });
 
  //Report route
         Route::prefix('reports')->group(function () {
-        Route::get('/', [ReportController::class, 'index'])->name('index');
-        Route::post('/', [ReportController::class, 'store'])->name('store');
-        Route::get('/{id}', [ReportController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ReportController::class, 'update'])->name('update');
-        Route::delete('/{id}', [ReportController::class, 'destroy'])->name('delete');
+        Route::get('/', [ReportController::class, 'index']);
+        Route::post('/', [ReportController::class, 'store']);
+        Route::get('/{id}', [ReportController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [ReportController::class, 'update']);
+        Route::delete('/{id}', [ReportController::class, 'destroy']);
 
     });
 
@@ -190,10 +190,10 @@ Route::as('bookstore.')->group(function () {
 
     //Orders Route
     Route::prefix('orders')->group(function (){
-        Route::get('/', [OrderController::class, 'index'])->name('index');
-        Route::post('/', [OrderController::class, 'store'])->name('store');
-        Route::get('/{invoiceNumber}', [OrderController::class, 'show'])->name('show');
-        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [OrderController::class, 'update'])->name('update'); 
+        Route::get('/', [OrderController::class, 'index']);
+        Route::post('/', [OrderController::class, 'store']);
+        Route::get('/{invoiceNumber}', [OrderController::class, 'show']);
+        Route::match(['POST', 'PUT', 'PATCH'], '/{id}', [OrderController::class, 'update']); 
         Route::delete('delete_order/{id}', [OrderController::class, 'deleteOrder'])->name('delete_order');
         Route::delete('delete_item/{id}', [OrderController::class, 'deleteOrderItem'])->name('delete_item');    
 
